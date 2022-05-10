32dc0989bb9c696f2fb55ea35abf7a0bcc8367a7
sokoving <sokoving@gmail.com>
2022-05-10 12:10:10 +0900

    0510 1219 js 사용자 선택 숫자 추출

--- 2. web-api/practice/un_down/app.js
+++ 2. web-api/practice/un_down/app.js
@@ -1,10 +1,21 @@
 //================== 전역변수, 함수 정의부 =================//
+//게임 진행에 필요한 데이터
+//(실제 정답, 선택한 숫자, 최솟값, 최댓값)
+const gameData = {
+    secret: Math.floor((Math.random()*100)+1),
+    answer: null,
+    min: 1,
+    max: 100
+};
+
+
+
 // 숫자 아이콘 생성 함수
 function makeIcons() {
 
     const $numbers = document.getElementById('numbers');
 
-    // 가상의 태그(최적화)
+    // 1. 가상의 태그(최적화)
     const $virtual = document.createDocumentFragment();
 
     for(let i = 1; i <= 100; i++){
@@ -14,7 +25,20 @@ function makeIcons() {
     $virtual.appendChild($newDiv);
     }
     $numbers.appendChild($virtual)
-}
+
+
+    // 2. 아이콘 클릭 이벤트 부여
+    $numbers.addEventListener('click', e => {
+        
+        // 만약에 아이콘을 클릭하지 않았다면 이벤트 x, 나가기
+        if (!e.target.matches('#numbers > .icon')) return;
+
+        // 사용자가 선택한 숫자가 무엇인가?
+        gameData.answer = +e.target.textContent;
+    
+    })
+
+}// end makeIcons
 
 
 
